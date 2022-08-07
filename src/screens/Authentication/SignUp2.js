2248544a729204bb0796db02e2a45c13e930b9c3
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2022-08-07 21:27:02 +0530

    signup2 added

---
+++ src/screens/Authentication/SignUp2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const SignUp2 = () => {
+  return (
+    <View><Text>SignUp2</Text></View>
+  )
+}
+
+export default SignUp2
--- src/stacks/Authenticationstack.js
+++ src/stacks/Authenticationstack.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import colors from '../constants/colors';
 import Login from '../screens/Authentication/Login';
+import Signup from '../screens/Authentication/SignUp';
+import SignUp2 from '../screens/Authentication/SignUp2';
 const Stack = createStackNavigator();
 
 const Authenticationstack = () => {
@@ -15,7 +17,9 @@ const Authenticationstack = () => {
         headerTintColor: colors.WHITE,
       }}
     >
-     <Stack.Screen name="Login" component={Login} />
+     {/* <Stack.Screen name="Login" component={Login} />
+     <Stack.Screen name="SignUp1" component={Signup} /> */}
+     <Stack.Screen name="SignUp2" component={SignUp2} />
 
     </Stack.Navigator>
   )
